c95726715680baa44d1fa184a7a983da29ea8faa
Adrian <gasinski.adrian@gmail.com>
2021-01-28 22:23:30 +0100

    create vector with filenames to import

--- ImportFiles.R
+++ ImportFiles.R
@@ -1,15 +1,10 @@
 ## set working directory
 setwd("C:/dev/repos/football_data/data")
 
-
-
-
-
-
+files_to_import <- c()
 for (directory in dir()) {
   for (filename in dir(path=dir)){
-    print(paste(directory, filename, sep="/"))
+    files_to_import <- c(files_to_import, paste(directory, filename, sep="/"))
   }
 }
-
 
